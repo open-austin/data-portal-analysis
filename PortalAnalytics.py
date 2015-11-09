563757ed8d5a308bf61dfbaf6f32d0ed41729f33
Marshall Barnes <mtb33@txstate.edu>
2015-11-09 14:47:10 -0600

    Refactoring: moving to object-oriented design, script still runs as before.

--- PortalAnalytics.py
+++ PortalAnalytics.py
@@ -1,15 +1,27 @@
+import sys
 import json
 
-data_json = open('datasets.json')
-data_str = data_json.read()
-data_dict = json.loads(data_str)
+class DatasetAnalyzer():
+    def __init__(self, filename):
+        data_json = open(filename)
+        data_str = data_json.read()
+        data_dict = json.loads(data_str)
+        self._data_dict = data_dict
 
-for item in data_dict['datasets']:
-    print item['id'], ':', item['name'], '\n'
-    for col in item['columns']:
-        print col['name']
-    print '\n\n'
+    def print_columns(self):
+        for item in self._data_dict['datasets']:
+            print item['id'], ':', item['name'], '\n'
+            for col in item['columns']:
+                print col['name']
 
+
+if __name__ == "__main__":
+    filename = "datasets.json"
+    if len(sys.argv) > 1:
+        filename = sys.argv[1]
+
+    Analyzer = DatasetAnalyzer(filename)
+    Analyzer.print_columns()
 
 
 
