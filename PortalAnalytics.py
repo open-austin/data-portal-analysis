18fc02499c289dd8651f90ee8ae60e4405552a29
Marshall Barnes <mtb33@txstate.edu>
2015-11-14 19:24:34 -0600

    analyze_dataset function refactored for readability.

--- PortalAnalytics.py
+++ PortalAnalytics.py
@@ -1,5 +1,7 @@
 import json
 import csv
+import sys
+
 
 class DatasetAnalyzer():
     def __init__(self, json_filename, output_filename):
@@ -10,7 +12,7 @@ class DatasetAnalyzer():
         with open(json_filename) as data_json:
             json_str = data_json.read()
             data_dict = json.loads(json_str)
-        self._datasets = data_dict['datasets'] # This will need to change
+        self._datasets = data_dict['datasets']  # This will need to change
 
         self._headers = ["id", "soc_resource_id", "dept", "name",
                          "col_position", "col_name", "col_field_name",
@@ -19,56 +21,6 @@ class DatasetAnalyzer():
                          "num_null", "num_not_null", "ex_value",
                          "snapshot_date_time", "is_current"]
 
-    def run(self):
-        csv_info = []
-        rows = []
-        csv_info.append(self._headers)
-        for item in self._datasets:
-            for row in self._analyze_dataset(item):
-                rows.append(row)
-        for number, row in enumerate(rows):
-            csv_info.append([number] + row)
-        return csv_info
-
-    def make_csv(self):
-        with open(self._outfile, "wb") as outfile:
-            writer = csv.writer(outfile, quoting=csv.QUOTE_MINIMAL)
-            for row in self.run():
-                writer.writerow(row)
-
-    def _get_department(self, dataset):
-        try:
-            dpt = dataset['metadata']['custom_fields']['Additional Information']['Department']
-        except:
-            dpt = "No Department Information"
-        return dpt.encode('utf-8')
-
-    def _get_null_count(self, col):
-        try:
-            total = col['cachedContents']['null']
-        except:
-            total = "None"
-        return total
-
-    def _get_non_null_count(self, col):
-        try:
-            total = col['cachedContents']['non_null']
-        except:
-            total = "None"
-        return total
-
-    def _get_top(self, col):
-        """This gets the top item from cachedContents.  
-        Currently the item is returned as a string representation,
-        which is less than ideal, but it works.
-
-        """
-        try:
-            top = col['cachedContents']['top'][0]['item']
-        except:
-            top = "None"
-        return repr(top)
-    
     def _analyze_dataset(self, dataset):
         """Analyze a dataset (dict) and return a list of rows.
         """
@@ -77,24 +29,92 @@ class DatasetAnalyzer():
         dataset_id = dataset['id']
         dataset_name = dataset['name'].encode('utf-8')
         dataset_dpt = self._get_department(dataset)
+        dataset_time = self._get_date_time(dataset)
 
-        for col_num, col in enumerate(dataset['columns']):
-            rows.append([dataset_id, dataset_dpt, dataset_name])
-            rows[col_num].append(col['position'])
-            rows[col_num].append(col['name'].encode('utf-8'))
-            rows[col_num].append(col['fieldName'].encode('utf-8'))
-            rows[col_num].append(col['id'])
-            rows[col_num].append(col['tableColumnId'])
-            rows[col_num].append(col['dataTypeName'].encode('utf-8'))
-            rows[col_num].append(col['renderTypeName'].encode('utf-8'))
-            rows[col_num].append(self._get_null_count(col))
-            rows[col_num].append(self._get_non_null_count(col))
-            rows[col_num].append(self._get_top(col))
-            rows[col_num].append("SNAPSHOT_DATE_TIME") # placeholder
-            rows[col_num].append("IS_CURRENT")         # placeholder
+        for col in dataset['columns']:
+            current_row = []
+            current_row += [dataset_id, dataset_dpt, dataset_name]
+            current_row += [col['position']]
+            current_row += [col['name'].encode('utf-8')]
+            current_row += [col['fieldName'].encode('utf-8')]
+            current_row += [col['id']]
+            current_row += [col['tableColumnId']]
+            current_row += [col['dataTypeName'].encode('utf-8')]
+            current_row += [col['renderTypeName'].encode('utf-8')]
+            current_row += [self._get_null_count(col)]
+            current_row += [self._get_non_null_count(col)]
+            current_row += [self._get_top(col)]
+            current_row += [dataset_time]
+            current_row += ["IS_CURRENT"]         # placeholder
+            rows.append(current_row)
+
         return rows
 
+    def _analyze_all(self):
+        """Run the analyzer on all datasets and return the results.
+        """
+        results = []
+        rows = []
+        results.append(self._headers)
+        for item in self._datasets:
+            for row in self._analyze_dataset(item):
+                rows.append(row)
+        for number, row in enumerate(rows):
+            results.append([number+1] + row)
+        return results
+
+    def _get_department(self, dataset):
+        try:
+            custom = dataset['metadata']['custom_fields']
+            dpt = custom['Additional Information']['Department']
+        except:
+            # print dataset['name']
+            dpt = "No Department Information"
+        return dpt.encode('utf-8')
+
+    def _get_null_count(self, col):
+        try:
+            total = col['cachedContents']['null']
+        except:
+            total = "null"
+        return total
+
+    def _get_non_null_count(self, col):
+        try:
+            total = col['cachedContents']['non_null']
+        except:
+            total = "null"
+        return total
+
+    def _get_top(self, col):
+        """This gets the top item from cachedContents.
+        Currently the item is returned as a python representation,
+        which is less than ideal, but it works.
+
+        """
+        try:
+            top = col['cachedContents']['top'][0]['item']
+        except:
+            top = "null"
+        return repr(top)
+
+    def _get_date_time(self, dataset):
+        try:
+            date = dataset['publicationDate']
+        except:
+            date = "null"
+        return date
+
+    def make_csv(self):
+        with open(self._outfile, "wb") as outfile:
+            writer = csv.writer(outfile, quoting=csv.QUOTE_MINIMAL)
+            for row in self._analyze_all():
+                writer.writerow(row)
+
+
 if __name__ == "__main__":
+    """USAGE: python PortalAnalytics.py <input file> <output file>
+    """
     filename = "datasets.json"
     outfile = "out.csv"
     if len(sys.argv) > 1:
