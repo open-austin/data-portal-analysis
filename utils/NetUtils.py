1080e450ce6f9533c7d1158b920e9b02cb835546
Hailey Pate <2haileypate@gmail.com>
2015-11-21 18:19:41 -0600

    add functions to SocIdGetter
    these functions fetch a list of current resources from the socrata data
    portal, filter out the stuff we don’t want, then deliver a nice tidy
    list of what we do want

--- utils/NetUtils.py
+++ utils/NetUtils.py
@@ -6,10 +6,52 @@ import logging
 
 class SocIdGetter:
     def __init__(self):
-        self._request_url = "http://data.austintexas.gov/search/views.json"
+        self._views_url = "https://data.austintexas.gov/views"
+        self._migrations_api = "https://data.austintexas.gov/api/migrations/"
+        self._view_metadata = self.get_all_view_ids()
+        self._dataset_ids = self.filter_view_ids(self._view_metadata)
 
-    def get_all_ids():
-        
+    def get_all_view_ids(self):
+        req = requests.get(self._views_url)
+        views_response_json = req.json()
+        view_metadata = []
+        for v in views_response_json:
+            try:
+                display_type = v['displayType']
+            except(KeyError): # if v represents a data_lens page, we ignore it
+                continue
+            soc_id = v['id']
+            view_type = v['viewType']
+            display_type = v['displayType']
+            item = [soc_id, view_type, display_type] # these attributes are used to filter out stuff we don't want
+            view_metadata.append(item)
+        return view_metadata    
+
+    def filter_view_ids(self, view_metadata):
+        dataset_ids = []
+        for v in view_metadata:
+            if v[1] == "tabular":
+                if v[2] == "table":
+                    dataset_ids.append(v)
+                else:
+                    continue
+            else:
+                continue
+        return dataset_ids
+
+    def filter_table_ids(self, dataset_ids):
+        soc_ids = [] # these will be the "new back end" ids of primary data assets (not derived views, etc)
+        for r in dataset_ids:
+            soc_id = r[0]
+            url = self._migrations_api + soc_id
+            req = requests.get(url)
+            response_json = req.json()
+            try:
+                i = response_json['nbeId']
+            except(KeyError):
+                continue
+            soc_ids.append(i)
+        return soc_ids
         
 class ViewRequestHandler:
     def __init__(self):
