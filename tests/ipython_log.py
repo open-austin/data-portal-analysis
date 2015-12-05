547d2a091b1e4361c1efa90f2ace5f70d51fe6c8
Marshall Barnes <mtb33@txstate.edu>
2015-12-04 19:15:55 -0600

    created remote branch for writing tests

---
+++ tests/ipython_log.py
@@ -0,0 +1,49 @@
+# IPython log file
+
+get_ipython().magic(u'logstart')
+get_ipython().magic(u'load_ext autoreload')
+get_ipython().magic(u'autorelod 2')
+get_ipython().magic(u'autoreload 2')
+from ..utilities import FileUtils
+import sys
+sys.path
+sys.path.append('/home/sata/source/open-austin')
+import utilities.FileUtils
+sys.path.append('/home/sata/source/open-austin/data-portal-analysis')
+import utilities.FileUtils
+import utilities
+reader = utilities.FileUtils.JsonFileReader('test_view_resource.json')
+reader.get_all_datasets()
+test_set = reader.get_all_datasets()
+test_set['new_backend']
+test_set[o]
+test_set[1]
+test_set[0]
+test_set[0]['next']
+test_set[0]['new_backend']
+test_set[0].keys()
+test_set = test_set[0]
+test_set['newBackend']
+test_set['id']
+sys.path
+__file__
+__name__
+__package__
+import test_file_utils
+import test_file_utils
+test_file_utils.printpath()
+test_file_utils.printpath()
+test_file_utils.printpath()
+test_file_utils.printpath()
+import test_file_utils
+test_file_utils.test_json_file_reader()
+test_file_utils.test_json_file_reader()
+test_file_utils.test_json_file_reader()
+test_file_utils.test_json_file_reader()
+testset
+test_set
+test_set['name']
+test_file_utils.test_json_file_reader()
+test_file_utils.test_json_file_reader()
+test_file_utils.test_json_file_reader()
+exit()
