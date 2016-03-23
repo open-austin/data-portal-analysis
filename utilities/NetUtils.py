f5e3164f99ba9992a6835cf7c8b76fbc2e891558
Marshall Barnes <mtb33@txstate.edu>
2016-03-23 12:14:38 -0500

    refactoring code in NetUtils module

--- utilities/NetUtils.py
+++ utilities/NetUtils.py
@@ -43,10 +43,15 @@ class SocIdGetter(object):
         return views_dict
 
 
-    def get_ids(self):
-        """Fetches views from Socrata, returns a collection of view metadata."""
-        views_dict = self._download_views()
-        view_metadata = []
+    def get_ids(self, viewfile=False):
+        """Fetches views from Socrata, returns a list of 4x4 socrata ids."""
+        if viewfile:
+            with open('views.json') as viewjson:
+                views_dict = json.loads(viewjson.read())
+        else:
+            views_dict = self._download_views()
+
+        id_list = []
         for view in views_dict['results']:
             try:
                 display_type = view['view']['displayType']
@@ -54,50 +59,29 @@ class SocIdGetter(object):
                 # this means the the view represents a data_lens page,
                 # so we ignore it
                 continue
-            soc_id = view['view']['id']
-            view_type = view['view']['viewType']
-            display_type = view['view']['displayType']
-            item = [soc_id, view_type, display_type]
-            # these attributes are used later on
-            # to filter out stuff we don't want
-            view_metadata.append(item)
+            if view['view']['viewType'] == "tabular":
+                if view['view']['displayType'] == "table":
+                    id_list.append(view['view']['id'])
 
-        tabular_ids = self.filter_view_ids(view_metadata)
-        fourby_list = self.filter_tabular_ids(tabular_ids)
-        return fourby_list
+        migrated_ids = [self._migrate_id(obe_id) for obe_id in id_list]
+        migrated_ids = [soc_id for soc_id in migrated_ids if soc_id is not None]
+        return id_list
 
-    @staticmethod
-    def filter_view_ids(view_metadata):
-        """Retrieves tabular data ids from the set of Socrata views."""
-        tabular_ids = []
-        for item in view_metadata:
-            if item[1] == "tabular":
-                if item[2] == "table":
-                    tabular_ids.append(item[0])
-                else:
-                    continue
-            else:
-                continue
-        return tabular_ids
-
-    def filter_tabular_ids(self, tabular_ids):
+    def _migrate_id(self, obe_id):
         """For each set of tabular data, fetch updated id from Socrata's
         migrations api.
         """
-        fourby_list = []
-        # these will be the "new back end" ids of primary data assets
-        for tab_id in tabular_ids:
-            url = self._migrations_api + tab_id
-            req = requests.get(url)
-            response_json = req.json()
-            try:
-                fourby_item = response_json['nbeId']
-            except(KeyError):
-                logging.info("{0} is not a primary data asset.".format(tab_id))
-                continue
-            else:
-                fourby_list.append(fourby_item)
-        return fourby_list
+        print "migrating {0}".format(obe_id)
+        url = self._migrations_api + obe_id
+        req = requests.get(url)
+        response_json = req.json()
+        try:
+            nbe_id = response_json['nbeId']
+        except(KeyError):
+            logging.info("{0} is not a primary data asset.".format(obe_id))
+            nbe_id = 'null'
+        else:
+            return nbe_id
 
 
 class ViewRequestHandler(object):
